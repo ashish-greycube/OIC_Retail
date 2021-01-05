7e9026c2b8df4be6974913170a1822cfd367c5e7
vijay_wm@yahoo.com <vijay_wm@yahoo.com>
2021-01-05 12:38:07 +0530

    fix: broken import in Expenses Statement report update prospects count in Monthly Outlet Target

--- oic_retail/oic_retail/doctype/monthly_outlet_target/monthly_outlet_target.js
+++ oic_retail/oic_retail/doctype/monthly_outlet_target/monthly_outlet_target.js
@@ -1,8 +1,17 @@
 // Copyright (c) 2021, greycube and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Monthly Outlet Target', {
-	// refresh: function(frm) {
+frappe.ui.form.on("Monthly Outlet Target", {
+  refresh: function (frm) {},
 
-	// }
+  sales_person: function (frm) {
+    //   set prospects on hand from Listed Retail Outlets for sales person
+    frappe.db
+      .count("Retail Outlet", {
+        filters: { outlet_status: "Listed" },
+      })
+      .then((count) => {
+        frm.set_value("prospects_on_hand", count);
+      });
+  },
 });
